e00977df6a8c995af50dd3c9b721686e5beedc7c
YunYouJun <me@yunyoujun.cn>
2020-02-01 19:08:00 +0800

    fix: config travis history

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,48 @@
 const pkg = require("../../package.json");
 
-module.exports = {
-  title: "ak-ui",
-  description: pkg.description,
-  head: [
-    ["link", { rel: "icon", href: "/ak.png" }],
-    ["link", { rel: "stylesheet", href: "/css/ak-ui.min.css" }]
-  ],
-  extraWatchFiles: [
-    ".vuepress/public/css/ak-ui.min.css" // 使用相对路径
-  ],
-  themeConfig: {
-    logo: "/gun.svg",
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Guide", link: "/guide/" },
-      { text: "Components", link: "/components/" }
-    ],
-    sidebar: {
-      "/components/": [
-        "",
-        "ak-button",
-        "ak-card",
-        "ak-fx",
-        "ak-loading"
+module.exports = ctx => {
+  return {
+    title: "ak-ui",
+    description: pkg.description,
+    head: [
+      ["link", { rel: "icon", href: "/ak.png" }],
+      [
+        "link",
+        {
+          rel: "stylesheet",
+          href: ctx.isProd ? "/css/ak-ui.min.css" : "/css/ak-ui.css"
+        }
       ]
+    ],
+    extraWatchFiles: [
+      ".vuepress/public/css/ak-ui.min.css" // 使用相对路径
+    ],
+    themeConfig: {
+      logo: "/gun.svg",
+      nav: [
+        { text: "Home", link: "/" },
+        { text: "Guide", link: "/guide/" },
+        { text: "Components", link: "/components/" }
+      ],
+      sidebar: {
+        "/components/": ["", "ak-button", "ak-card", "ak-fx", "ak-loading"]
+      },
+      lastUpdated: "Last Updated",
+      repo: "YunYouJun/ak-ui",
+      docsDir: "docs",
+      editLinks: true,
+      editLinkText: "帮助改善此页面！( ￣□￣)/",
+      smoothScroll: true
     },
-    lastUpdated: "Last Updated",
-    repo: "YunYouJun/ak-ui",
-    docsDir: "docs",
-    editLinks: true,
-    editLinkText: "帮助改善此页面！( ￣□￣)/",
-    smoothScroll: true
-  },
-  plugins: [
-    "@vuepress/back-to-top",
-    "@vuepress/medium-zoom",
-    [
-      "@vuepress/google-analytics",
-      {
-        ga: "UA-121354150-11"
-      }
+    plugins: [
+      "@vuepress/back-to-top",
+      "@vuepress/medium-zoom",
+      [
+        "@vuepress/google-analytics",
+        {
+          ga: "UA-121354150-11"
+        }
+      ]
     ]
-  ]
+  };
 };
