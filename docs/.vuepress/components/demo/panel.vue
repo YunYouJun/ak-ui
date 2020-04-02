e640d8ef4e1557ccd69ad81cd801ae78300df55e
YunYouJun <me@yunyoujun.cn>
2020-04-02 18:57:30 +0800

    :lipstick: ak-button-group--action total

--- docs/.vuepress/components/demo/panel.vue
+++ docs/.vuepress/components/demo/panel.vue
@@ -2,5 +2,8 @@
   <div class="ak-panel-right">
     <demo-divider style="width: 30rem"></demo-divider>
     <demo-counter></demo-counter>
+    <demo-button-group-action
+      style="margin-top: 150px"
+    ></demo-button-group-action>
   </div>
 </template>
