e0516e54bc0043fcd33c971d18bc14d9f419f563
YunYouJun <me@yunyoujun.cn>
2020-04-10 00:27:06 +0800

    :sparkles: ak-san

---
+++ docs/.vuepress/components/demo/san.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="background:rgba(255,255,255,0.8);padding:2rem;">
+    <div class="ak-san">
+      <div class="brain">
+        <svg class="ak-icon" aria-hidden="true">
+          <use xlink:href="#icon-Brain"></use>
+        </svg>
+      </div>
+      <div class="add">
+        <svg class="ak-icon" aria-hidden="true">
+          <use xlink:href="#icon-add-line"></use>
+        </svg>
+      </div>
+      <div class="cur"><div class="value">125</div></div>
+      <div class="total">理智/130</div>
+    </div>
+  </div>
+</template>
